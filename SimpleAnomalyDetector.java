b305b81226f86efe71158b9362cc8aae4700ec68
Oron Pariente <89079384+oronp@users.noreply.github.com>
2021-12-03 12:13:52 +0200

    TimeSeries update 3.12

---
+++ SimpleAnomalyDetector.java
@@ -0,0 +1,20 @@
+package test;
+import java.util.List;
+
+public class SimpleAnomalyDetector implements TimeSeriesAnomalyDetector {
+	
+
+	@Override
+	public void learnNormal(TimeSeries ts) {
+	}
+
+
+	@Override
+	public List<AnomalyReport> detect(TimeSeries ts) {
+		return null;
+	}
+	
+	public List<CorrelatedFeatures> getNormalModel(){
+		return null;
+	}
+}
---
+++ TimeSeries.java
@@ -0,0 +1,38 @@
+package test;
+import java.io.*;
+import java.util.*;
+
+public class TimeSeries {
+	HashMap<String,Vector<Double>> timeTable = new HashMap<>();
+	int numberOfColumns;
+
+	public TimeSeries(String csvFileName) throws FileNotFoundException {
+		Scanner scanner;
+		String fullLine = null;
+		String[] headers = null;
+		String[] numbers = null;
+		try{
+			scanner = new Scanner(new File(csvFileName));
+			//reading the first line of the csv file -> seperating the columns and saving the names in a string array.
+			fullLine = scanner.next();
+			headers = fullLine.split(",");
+			this.numberOfColumns = headers.length; //updating the number of columns according to the number of headlines...
+
+			for(int i = 0; i < numberOfColumns; i++){
+				Vector<Double> vec = new Vector<>();
+				timeTable.put(headers[i],vec);
+			} //iniziallize all the collumns of the maps with the name and empty vector.
+
+			while(scanner.hasNext()){
+				fullLine = scanner.next();
+				numbers = fullLine.split(",");
+				for(int i = 0; i < numberOfColumns; i++){
+					timeTable.get(headers[i]).add(Double.parseDouble(numbers[i]));
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
